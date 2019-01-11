2fa6a2e78a91e897eb860d9e5303fcdf601e40cc
theTuring <46594931+theTuring@users.noreply.github.com>
2019-01-12 02:02:24 +0800

    first commit

---
+++ src/main/java/com/console/dao/AdminsDao.java
@@ -0,0 +1,130 @@
+package com.console.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.console.db.DBUtil;
+import com.console.entity.Admins;
+import com.console.entity.Users;
+
+public class AdminsDao {
+
+    //全部查询
+    public List<Admins> query() throws SQLException{
+
+        List<Admins> adminsList = new ArrayList<Admins>();
+
+        //获取数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("select * from admin");
+
+        // 通过数据库的连接操作数据库，实现增删改查
+        PreparedStatement ptmt = conn.prepareStatement(sb.toString());
+
+        ResultSet rs = ptmt.executeQuery();
+
+        Admins admins = null;
+
+        while(rs.next()){
+
+            admins = new Admins();
+
+            admins.setId(rs.getInt("id"));
+            admins.setUsername(rs.getString("username"));
+            admins.setPassword(rs.getString("password"));
+
+            adminsList.add(admins);
+
+        }
+
+        return adminsList;
+
+    }
+
+    //单个查询
+    public Admins queryById(Integer id) throws SQLException{
+
+        Admins u = null;
+
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "select * from admin where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setInt(1,id);
+
+        ResultSet rs = ptmt.executeQuery();
+
+        while(rs.next()){
+
+            u = new Admins();
+
+            u.setId(rs.getInt("id"));
+            u.setUsername(rs.getString("username"));
+            u.setPassword(rs.getString("password"));
+
+        }
+
+        return u;
+    }
+
+    //添加功能
+    public void addAdmins(Admins admins) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "insert into admin(username,password)values(?,?)";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setString(1,admins.getUsername());
+        ptmt.setString(2,admins.getPassword());
+
+        ptmt.execute();
+
+    }
+
+    //修改管理员
+    public void updateAdmins(Admins admins) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "update admin set username=?,password=?where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setString(1,admins.getUsername());
+        ptmt.setString(2,admins.getPassword());
+
+        ptmt.setInt(3,admins.getId());
+
+        ptmt.execute();
+
+    }
+
+    //删除管理员
+    public void deleteAdmins(Integer id) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "delete from admin where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setInt(1, id);
+
+        ptmt.execute();
+
+    }
+}
---
+++ src/main/java/com/console/dao/UsersDao.java
@@ -0,0 +1,137 @@
+package com.console.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.console.db.DBUtil;
+import com.console.entity.Users;
+
+public class UsersDao {
+
+    //全部查询
+    public List<Users> query() throws SQLException{
+
+        List<Users> usersList = new ArrayList<Users>();
+
+        //获取数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("select * from user");
+
+        // 通过数据库的连接操作数据库，实现增删改查
+        PreparedStatement ptmt = conn.prepareStatement(sb.toString());
+
+        ResultSet rs = ptmt.executeQuery();
+
+        Users users = null;
+
+        while(rs.next()){
+
+            users = new Users();
+
+            users.setId(rs.getInt("id"));
+            users.setUsername(rs.getString("username"));
+            users.setPassword(rs.getString("password"));
+            users.setName(rs.getString("name"));
+            users.setGender(rs.getString("gender"));
+            users.setPhone(rs.getString("phone"));
+
+            usersList.add(users);
+
+        }
+
+        return usersList;
+    }
+
+    //单个查询
+    public Users queryById(Integer id) throws SQLException{
+
+        Users u = null;
+
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "select * from user where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setInt(1,id);
+
+        ResultSet rs = ptmt.executeQuery();
+
+        while(rs.next()){
+
+            u = new Users();
+
+            u.setId(rs.getInt("id"));
+            u.setUsername(rs.getString("username"));
+            u.setPassword(rs.getString("password"));
+            u.setName(rs.getString("name"));
+            u.setGender(rs.getString("gender"));
+            u.setPhone(rs.getString("phone"));
+
+        }
+
+        return u;
+    }
+
+    //添加功能
+    public void addUsers(Users users) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "insert into users(username,password,name,gender,phone)values(?,?,?,?,?)";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setString(1,users.getUsername());
+        ptmt.setString(2,users.getPassword());
+        ptmt.setString(3,users.getName());
+        ptmt.setString(4,users.getGender());
+        ptmt.setString(5,users.getPhone());
+
+        ptmt.execute();
+    }
+
+    //修改用户资料
+    public void updateUsers(Users users) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "update user set username=?,password=?,name=?,gender=?,phone=? where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setString(1,users.getUsername());
+        ptmt.setString(2,users.getPassword());
+        ptmt.setString(3,users.getName());
+        ptmt.setString(4,users.getGender());
+        ptmt.setString(5,users.getPhone());
+
+        ptmt.setInt(6,users.getId());
+
+        ptmt.execute();
+    }
+
+    //删除用户
+    public void deleteUsers(Integer id) throws SQLException{
+
+        // 获得数据库连接
+        Connection conn = DBUtil.getConnection();
+
+        String sql = "delete from user where id=?";
+
+        PreparedStatement ptmt = conn.prepareStatement(sql);
+
+        ptmt.setInt(1, id);
+
+        ptmt.execute();
+
+    }
+}
